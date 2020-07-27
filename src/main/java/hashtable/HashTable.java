1185c9b3f6d1c4bf270148a1248337f50e4e188d
rohit nagappa <rnagappa35@gmail.com>
2020-07-27 23:17:11 +0530

    implmented hashtable

---
+++ src/main/java/hashtable/HashTable.java
@@ -0,0 +1,80 @@
+package hashtable;
+
+public class HashTable<K,V> {
+
+	private static final int SIZE = 10;
+	
+	private static class HashEntry<K,V>{
+		K key;
+		V value;
+		
+		HashEntry<K,V> next;
+		
+		public HashEntry(K k,V v) {
+			
+			this.key = k;
+			this.value = v;
+			this.next = null;
+		}
+		@Override
+		public String toString() {
+			return "HashEntry {key=" + key + ", value=" + value + ", next=" + next + "}";
+		}
+	}
+	
+	
+	private final HashEntry[] entries = new HashEntry[SIZE];
+	
+	
+	private int getHash(K key) {
+		return Math.abs(key.hashCode()%SIZE);
+	}
+	public void put(K key,V value) {
+		
+		int hash = getHash(key);
+		
+		final HashEntry hashentry = new HashEntry(key,value);
+		
+		if(entries[hash] == null) {
+			entries[hash] = hashentry;
+		}else { //collission chaining
+			
+			HashEntry currentEntry = entries[hash];
+			
+			while(currentEntry.next != null) {
+				currentEntry = currentEntry.next;
+			}
+			
+			currentEntry.next = hashentry;
+			
+			
+		}
+		
+		
+	}
+	
+    public V get(K key) {
+
+        int hash = getHash(key);
+        
+        if (entries[hash] != null) {
+            HashEntry currentEntry = entries[hash];
+
+            // Check the entry linked list for matching the given 'key'
+            while (currentEntry != null) {                
+
+                if (currentEntry.key.equals(key)) {
+                    return (V) currentEntry.value;
+                }
+                
+                currentEntry = currentEntry.next;
+            }
+        }
+
+        return null;
+    }
+	
+	
+	
+		
+}
